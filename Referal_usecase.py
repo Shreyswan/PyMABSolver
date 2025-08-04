2db0900dd525c501ca14f450f7439b00538a489a
Shreyas Sawant <119746409+Shreyswan@users.noreply.github.com>
2025-08-04 17:31:01 +0530

    Added Referral codes

---
+++ Referal_usecase.py
@@ -0,0 +1,10 @@
+import PyMABSolver as pm
+
+sol = pm.MABSolver(n=5, t=500, tf=200, eps=0.01)
+
+avg_rew_list_expr, rewards = sol.exploration()
+avg_rew_list_expl, rewards = sol.exploitation()
+avg_rew_list_fege, rewards = sol.fixed_exploration_greedy_exploitation()
+avg_rew_list_eps, rewards = sol.epsilon_greedy()
+
+sol.plot_comparison(avg_rew_list_expr=avg_rew_list_expr, avg_rew_list_expl=avg_rew_list_expl, avg_rew_list_fege=avg_rew_list_fege, avg_rew_list_eps=avg_rew_list_eps)
